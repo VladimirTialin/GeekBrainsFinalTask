5e0463d132c076bbcd19d44054c2a5e8446d35a2
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2023-02-24 04:43:40 +0300

    task 8 finish new update

--- WorkMySQL/Animalsdb.sql
+++ WorkMySQL/Animalsdb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE Human_friends;
 USE Human_friends;
-
+-- End Task 7
 CREATE TABLE Animals(
     AnimalsId INT PRIMARY KEY AUTO_INCREMENT, 
     Pet VARCHAR(20) UNIQUE NOT NULL
@@ -40,3 +40,84 @@ VALUES
 	(2, 'CameL'); 
 
 SELECT * FROM Pets, PackAnimals;
+-- End Task 8
+
+INSERT Commands(name)
+VALUES
+	('Сидеть'),
+    ('Лежить'),
+    ('Голос'),
+    ('Вперед'),
+    ('Стоять'),
+    ('Кушать'),
+    ('Но! - пошёл'),
+    ('Тр-р-р-р - трмози'),
+    ('хать-хать!'),
+    ('Служи'),
+    ('Кувырок'),
+    ('Грызи'),
+    ('Гит'),
+    ('Дур'),
+    ('Каш'),
+    ('Цок, Цок!');    
+SELECT * FROM Commands;
+
+-- Создаем таблицу домашних животных
+CREATE TABLE Cat(
+    CatId INT PRIMARY KEY AUTO_INCREMENT,
+    PetsId INT,
+    FOREIGN KEY (PetsId) REFERENCES Pets(PetId),
+    Name VARCHAR(20) NOT NULL,
+    BirthDay DATE NOT NULL,
+    CommandId INT NOT NULL,
+    FOREIGN KEY (CommandId) REFERENCES Commands(CommandsId)
+);
+CREATE TABLE Dog(
+    DogId INT PRIMARY KEY AUTO_INCREMENT,
+    PetsId INT,
+    FOREIGN KEY (PetsId) REFERENCES Pets(PetId),
+    Name VARCHAR(20) NOT NULL,
+    BirthDay DATE NOT NULL,
+    CommandId INT NOT NULL,
+    FOREIGN KEY (CommandId) REFERENCES Commands(CommandsId)
+);
+CREATE TABLE Hamster(
+    HamsterId INT PRIMARY KEY AUTO_INCREMENT,
+    PetsId INT,
+    FOREIGN KEY (PetsId) REFERENCES Pets(PetId),
+    Name VARCHAR(20) NOT NULL,
+    BirthDay DATE NOT NULL,
+    CommandId INT NOT NULL,
+    FOREIGN KEY (CommandId) REFERENCES Commands(CommandsId)
+);
+-- Создаем таблицу вьючных животных
+CREATE TABLE Horse(
+    HorseId INT PRIMARY KEY AUTO_INCREMENT,
+    PactsId INT,
+    FOREIGN KEY (PactsId) REFERENCES PackAnimals(PackId),
+    Name VARCHAR(20) NOT NULL,
+    BirthDay DATE NOT NULL,
+    CommandId INT NOT NULL,
+    FOREIGN KEY (CommandId) REFERENCES Commands(CommandsId)
+);
+
+CREATE TABLE Donkey(
+    DonkeyId INT PRIMARY KEY AUTO_INCREMENT,
+	PactsId INT,
+    FOREIGN KEY (PactsId) REFERENCES PackAnimals(PackId),
+    Name VARCHAR(20) NOT NULL,
+    BirthDay DATE NOT NULL,
+    CommandId INT NOT NULL,
+    FOREIGN KEY (CommandId) REFERENCES Commands(CommandsId)
+);
+
+CREATE TABLE Camel(
+    CamelId INT PRIMARY KEY AUTO_INCREMENT,
+	PactsId INT,
+    FOREIGN KEY (PactsId) REFERENCES PackAnimals(PackId),
+    Name VARCHAR(20) NOT NULL,
+    BirthDay DATE NOT NULL,
+    CommandId INT NOT NULL,
+    FOREIGN KEY (CommandId) REFERENCES Commands(CommandsId)
+);
+-- End Task 8
